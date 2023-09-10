aa75595f4164b435cb32fbe67f7a532602c3734c
cogi2996 <bidang.tuyloan@gmail.com>
2023-09-10 15:58:26 +0700

    'add'

--- test.go
+++ test.go
@@ -1,26 +1,39 @@
-// You can edit this code!
-// Click here and start typing.
 package main
 
 import "fmt"
 
-func add(x,y int64) int64{
-    return  x+ y
+func setMap(numbers map[string]int) {
+    numbers["A"] = 1
+    numbers["B"] = 2
+    numbers["C"] = 3
+    numbers["D"] = 4
+    numbers["E"] = 5
+    numbers["B"] = 5
 }
 
-func mul(x,y int64) int64{
-    return x*y
+type Move interface{
+    move() string
+}
+type Car struct {
+    name string 
 }
 
-func cal(x,y int64,fn func(int64,int64)int64)int64{
-    a := x*10
-    var b int64
-    b = y*10
-    return fn(a,b)
+type Animal struct{
+    name string
 }
+
+func (car Car) move() string{
+    return "Moving by wheel"
+}
+
+func (a Animal) move() string{
+    return "Moving by leg"
+}
+
+
+
 func main() {
-    x :=cal(10,20,add)
-    y := cal(10,20,mul)
-    fmt.Print(x,y)
-    
+
+    car1 := Move{Car{"Mescedes"}}
+
 }
